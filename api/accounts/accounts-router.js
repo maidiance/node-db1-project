28882a2f47d400dcc3df2aed1621c3124b37e887
Winifred Lu <winnie.lu@comcast.net>
2022-02-14 15:03:25 -0500

    basic get endpoints complete

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,11 +1,25 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model');
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.status(500).json({message: 'could not get accounts'});
+    });
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  Accounts.getById(id)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.status(500).json({message: 'could not get account'});
+    })
 })
 
 router.post('/', (req, res, next) => {
